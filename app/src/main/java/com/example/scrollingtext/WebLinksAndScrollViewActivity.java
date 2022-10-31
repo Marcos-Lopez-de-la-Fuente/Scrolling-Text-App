492abb488638424613aa3ad747bb4b065c9773c7
Marcos López <74747123+Marcos-Lopez-de-la-Fuente@users.noreply.github.com>
2022-10-31 17:52:10 +0100

    Primeros avances del trabajo

---
+++ app/src/main/java/com/example/scrollingtext/WebLinksAndScrollViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.scrollingtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebLinksAndScrollViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_links_and_scroll_view);
+    }
+}
